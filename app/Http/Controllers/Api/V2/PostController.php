32c51d2b9e4334f67dd325c7a5807fba2dfd86c9
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2022-10-22 17:03:52 +0100

    Added Delete Request to Post Controller

--- app/Http/Controllers/Api/V2/PostController.php
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -70,8 +70,8 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        return $post->delete();
     }
 }
